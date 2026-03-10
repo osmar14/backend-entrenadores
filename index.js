66d3d6736b70836367b2ffec9111b72966b88e88
Osmar <ramso142426@gmail.com>
2026-03-09 20:36:03 -0600

    Conexión a base de datos interna de Railway

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+const db = mysql.createPool({
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'ZCwKXEEmhdNENCbvuqhPAHhCGlywgQEh',
+  database: 'railway',
+  port: 3306
 });
 
 
